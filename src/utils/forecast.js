85eec979c2ce6c6a438f248759049874576b4eb8
karan658 <f20190872@pilani.bits-pilani.ac.in>
2020-07-22 16:39:48 +0530

    added degree celsuis

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
 
-    const url = 'http://api.weatherstack.com/current?access_key=f164f4a600e231f187479dbd516dee3e&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=f164f4a600e231f187479dbd516dee3e&query=' + latitude + ',' + longitude 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
 
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('unable to find location', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is ' + body.current.temperature + ' and it feels like ' + body.current.feelslike + '. The humidity is ' + body.current.humidity);
+            callback(undefined, body.current.weather_descriptions[0] + ' .It is ' + body.current.temperature + 'degree Celsius and it feels like ' + body.current.feelslike + 'degree Celsius. The humidity is ' + body.current.humidity);
 
         }
         // console.log(body.current.weather_descriptions[0]+ 'It is' +body.current.temperature + 'and it feels like' +body.current.feelslike );
